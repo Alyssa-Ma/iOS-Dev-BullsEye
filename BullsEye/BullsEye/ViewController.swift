10bc3455aa1d639bcf7f8e8afe44da122ef7d037
Alyssa Ma <Alyssa.Ma80@myhunter.cuny.edu>
2021-03-29 18:16:03 -0400

    slider ui

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -25,6 +25,28 @@ class ViewController: UIViewController {
     
     //runs only on app startup
     override func viewDidLoad() {
+        
+        //slider visuals
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(
+            top: 0,
+            left: 14,
+            bottom: 0,
+            right: 14)
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
         super.viewDidLoad()
         //start a new game round
         startNewGame()
